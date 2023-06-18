5b6b54d9b8b374a3e13bffbf2705428831486163
Yan <Exm230600@gmail.com>
2023-06-18 23:46:46 +0200

    changes and add full work page of posts

--- components/posts-page-component.js
+++ components/posts-page-component.js
@@ -5,7 +5,9 @@ import { posts, goToPage } from "../index.js";
 export function renderPostsPageComponent({ appEl }) {
   // TODO: реализовать рендер постов из api
   console.log("Актуальный список постов:", posts);
-
+ if (posts.length === 0){
+  return
+ }
   /**
    * TODO: чтобы отформатировать дату создания поста в виде "19 минут назад"
    * можно использовать https://date-fns.org/v2.29.3/docs/formatDistanceToNow
